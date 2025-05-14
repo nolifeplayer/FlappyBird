f7158e520951a4510bf2bffba1470030a9e59bfa
nolifeplayer <KAig19@nycstudents.net>
2025-05-14 09:16:18 -0400

    Restart button

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
 public class BirdController : MonoBehaviour
 {
    
+    public TextMeshProUGUI gameOverText;
     Rigidbody2D bird;
     int score = 0;
     public Text scoreUI;
+    //True OR False value to check if bird is alive or not
+    public bool isAlive;
     // Start is called before the first frame update
     void Start()
     {
         bird = GetComponent<Rigidbody2D>();
+         //Set the alive to true
+        isAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+       
+        
+        if(Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             bird.AddForce(new Vector2(0,1) * 200);
+        }
+        if(isAlive == false)
+        {
+            GameOver();
         }
     }
     void OnTriggerEnter2D(Collider2D other)
@@ -31,5 +46,21 @@ public class BirdController : MonoBehaviour
 
             scoreUI.text = score.ToString();
         }
+    }
+    //Check when the player collides with something in the scene
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //set isAlive variable  to false when a collision happens with the player
+        isAlive = false;
+    }
+   
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    
+    BirdController birdControllerScript;
     public GameObject pipePrefab;
     float randomHeight = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnPipes" ,1.5f, 1.5f);
+        birdControllerScript = GameObject.Find("Red_Bird_1").GetComponent<BirdController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if isAlive variable from PlayerController is false
+        if(birdControllerScript.isAlive == false)
+        {
+            // Stop Spawning in new pipes
+            CancelInvoke();
+        }
     }
 
     void SpawnPipes()
