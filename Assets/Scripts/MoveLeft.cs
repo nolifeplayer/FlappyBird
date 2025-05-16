b437e809cff3b1154e351904d5aa9f2275aeb504
nolifeplayer <KAig19@nycstudents.net>
2025-05-16 09:06:21 -0400

    Pipes and Background

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    // Creatubg a variable to store the BirdController script
+    BirdController birdControllerScript;
     public float speed = 2;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        //Find the player controller script when the game Starts
+        birdControllerScript = GameObject.Find("Red_Bird_1").GetComponent<BirdController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Move pipes left
-        transform.Translate(Vector2.left * Time.deltaTime * speed);
+        if(birdControllerScript.isAlive == true)
+        {
+             transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+        else
+        {
+            speed = 0;
+        }
+        
     }
 }
